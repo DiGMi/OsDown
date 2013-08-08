942d016a5a889fb6ae6b95ebed3024a9caa8e462
Or Dagmi <digmi1@gmail.com>
2013-08-08 14:32:35 +0300

    Adding all the files

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ OsDown
 ======
 
 A script that downloads subtitles from opensubtitles automatically for a given language
+
+Requirements
+============
+The script requires the python-opensubtitles module which can be found at: https://github.com/agonzalezro/python-opensubtitles
+
+Usage
+=====
+First, edit the osdown.cfg file and add your username and password in the Login section.
+You can also select your default language id (currently set to heb - for hebrew)
+When running 'python osdown.py <filepath>' the script will download the first subtitle found.
+
---
+++ osdown.py
@@ -0,0 +1,59 @@
+from pythonopensubtitles.opensubtitles import OpenSubtitles
+from pythonopensubtitles.utils import File
+import ConfigParser
+import urllib
+from tempfile import mktemp
+from gzip import GzipFile
+import os
+
+
+config = ConfigParser.RawConfigParser()
+config.read('osdown.cfg')
+osmgr = OpenSubtitles()
+osmgr.login(config.get('Login', 'user'), config.get('Login', 'pass'))
+
+
+def find_subtitles(path, langid='all'):
+    ''' Get a list of subtitles for a given file '''
+    f = File(path)
+    hash = f.get_hash()
+    assert type(hash) == str
+    size = f.size
+    assert type(size) == int
+    data = osmgr.search_subtitles([{'sublanguageid': langid,
+                                   'moviehash': hash,
+                                   'moviebytesize': size}])
+    assert type(data) == list
+    return data
+
+
+def download_subtitle(subtitle):
+    print 'Downloading %s...' % subtitle['SubFileName']
+    tempfile = mktemp()
+    urllib.urlretrieve(subtitle['SubDownloadLink'], tempfile)
+    gzip = GzipFile(tempfile)
+    with open(subtitle['SubFileName'], 'wb') as f:
+        f.write(gzip.read())
+    os.remove(tempfile)
+
+
+def usage():
+    import sys
+    print "%s <filename> [<langid>]" % sys.argv[0]
+
+
+def main():
+    import sys
+    if len(sys.argv) < 2:
+        usage()
+        return
+    langid = config.get('Language', 'langid')
+    if len(sys.argv) >= 3:
+        langid = sys.argv[2]
+    subtitles = find_subtitles(sys.argv[1], langid)
+    download_subtitle(subtitles[0])
+
+
+
+if __name__ == '__main__':
+    main()
